970059be22e0557044307f76f6f05aba76a4d9f4
computerLearn <37542461+computerLearn@users.noreply.github.com>
2018-03-27 22:03:25 +0800

    return 01
    return 01

--- src/com/aiguigu/github/test/Test01.java
+++ src/com/aiguigu/github/test/Test01.java
@@ -3,5 +3,6 @@ package com.aiguigu.github.test;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器");
+		System.out.println("ok,github服务器 return");
 	}
 }
